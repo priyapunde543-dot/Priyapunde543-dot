52efbd51e50e40ba793caf7045f8e66fdeafe3f4
Priya <priyapunde543@gmail.com>
2026-07-12 10:31:51 +0530

    Create Array-DSA.py

---
+++ Array-DSA.py
@@ -0,0 +1,35 @@
+#problem 1 
+# ARRAY LiST
+a= [10, 20, 30, 40]
+# index at 0 and 3 find
+print(a[0])
+print(a[2])
+# problem 2 all index travel
+arr = [10,20,30,40]
+for i in arr:
+    print(i)
+#problem 3 insert element
+  arr = [10,20,30,40]
+arr.append(50)
+print(arr)
+#problem 4 insert at position
+arr = [10,20,30,40]
+arr.insert(3,45)
+print(arr)
+#problem 5 Delete element
+arr = [10,20,30,40]
+arr.remove(40)
+print(arr)
+#problem 6 length 
+arr = [10,20,30]
+print(len(arr))
+#problem 7 maximum
+arr = [8,2,15,4]
+print(max(arr))
+#problem 8 minimum
+arr = [8,2,15,4]
+print(min(arr))
+#problem 9 reverse
+arr = [1,2,3,4]
+arr.reverse()
+print(arr)
